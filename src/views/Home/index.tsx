8c9d39d236b2f458e3fd2a01709d84bb564a495c
Thomas Markiewicz <tomek@librem.one>
2020-10-15 15:00:00 -0500

    Moved CNAME to Public so it gets published on gh pages

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { Text} from '../../components/Text';
 
 export const Home = () => (
-    <Text>Work in progress on the future home of Calma Foods!</Text>
+    <Text>Work in progress on the future home of Calma Foods.</Text>
 );
 
